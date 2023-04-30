df7f14520617558bd5dada0d964b945bdb386cfa
pexApred <120075705+pexApred@users.noreply.github.com>
2023-04-30 02:15:11 -0400

    imported and synced sequelize

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
+sequelize.sync({ force: false }).then(() => {
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
