3cd1cdc9cf8bc6cf61250bcb2d76f3cf1c492cb9
pexApred <120075705+pexApred@users.noreply.github.com>
2023-04-30 09:47:52 -0400

    edited Category Model

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { INTEGER } = require('sequelize');
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
